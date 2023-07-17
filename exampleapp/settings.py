d66cb168898ec43466712f9032d1cce7886ca451
Eric Sheen <eric.sprog@gmail.com>
2023-07-17 16:46:17 -0400

    remove extra session middleware

--- exampleapp/settings.py
+++ exampleapp/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django_descope.middleware.DescopeMiddleware',
 ]
